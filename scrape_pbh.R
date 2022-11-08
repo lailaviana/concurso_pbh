241cbab5467752dd5299f00302abe0a3ca22ca8b
Laila Viana <lailavalmeida@gmail.com>
2022-11-08 14:49:21 -0300

    Initial commit

---
+++ scrape_pbh.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(xml2)
+library(rvest)
+
+url <- "https://prefeitura.pbh.gov.br/saude/oportunidades-de-trabalho/concurso-publico-01-2020"
+
+r_pbh <- url |> 
+  xml2::read_html() |> 
+  rvest::html_nodes(xpath="//*[@class='main-content-inner']") |> 
+  rvest::html_table()
+
+r_pbh_final <- r_pbh[[1]] |> dplyr::select(1,4)
